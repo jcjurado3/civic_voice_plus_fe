0ca0efd0dfeaadac7a6c8c053e783470392bcda6
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-02 14:51:02 -0600

    refactor variable

--- spec/facades/category_facade_spec.rb
+++ spec/facades/category_facade_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe CategoryFacade do
   describe "class methods" do
     describe "#get_all_categories" do
       it "returns all categories", :vcr do
-        user_categories = CategoryFacade.new.get_all_categories
+        categories = CategoryFacade.new.get_all_categories
 
-        expect(user_categories).to be_an(Array)
-        expect(user_categories.count).to eq(16)
+        expect(categories).to be_an(Array)
+        expect(categories.count).to eq(16)
 
-        expect(user_categories.first).to be_an(Category)
-        expect(user_categories.first.id).to be_a(String)
-        expect(user_categories.first.id).to eq("1")
-        expect(user_categories.first.name).to be_a(String)
-        expect(user_categories.first.name).to eq("artificial intelligence")
+        expect(categories.first).to be_an(Category)
+        expect(categories.first.id).to be_a(String)
+        expect(categories.first.id).to eq("1")
+        expect(categories.first.name).to be_a(String)
+        expect(categories.first.name).to eq("artificial intelligence")
       end
     end
   end  
